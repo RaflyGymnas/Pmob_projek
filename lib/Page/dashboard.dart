1da7e2faca63fc0b7ac3eff9cecbd5dcc16973bb
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-02-20 16:44:24 +0700

    Ini adalah Projek Mobile

---
+++ lib/Page/dashboard.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/rendering/box.dart';
+
+
+void main() {
+  runApp(dashboardpage());
+}
+
+class dashboardpage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home : Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          toolbarHeight: 100,
+        
+          actions: [
+            ElevatedButton(onPressed: (){
+              
+            },style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white
+            ), 
+            child: 
+            CircleAvatar(),
+            ),
+            
+          ],
+          title: Padding(padding: 
+          const EdgeInsets.fromLTRB(0, 0, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(left:0),
+                      child: Text("Halo, User!!",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 22,
+                              //fontFamily: "Montserrat",
+                              fontWeight: FontWeight.w700)),
+                    ),
+            ],
+          ),
+            ],
+          )
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(35, 0, 200, 0),
+            child: Text('Apa Kabar',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.w700
+            ),
+            ),
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(40, 25, 40, 10),
+              child: TextField(),
+            ),
+          ),
+        ],
+      ),
+      )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:projek_pmob/Page/dashboard.dart';
+import 'package:flutter/src/rendering/box.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return dashboardpage();
+  }
+}
