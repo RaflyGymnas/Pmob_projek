d461133e18b1967ea61ec37585200227c64d6319
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-02-21 10:33:25 +0700

    oke

--- lib/Page/dashboard.dart
+++ lib/Page/dashboard.dart
@@ -16,7 +16,7 @@ class dashboardpage extends StatelessWidget {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
-          toolbarHeight: 100,
+          toolbarHeight: 80,
         
           actions: [
             ElevatedButton(onPressed: (){
@@ -57,21 +57,110 @@ class dashboardpage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.fromLTRB(35, 0, 200, 0),
-            child: Text('Apa Kabar',
+            child: Text('Apa Kabar?',
             style: TextStyle(
-              fontSize: 32,
+              fontSize: 30,
               fontWeight: FontWeight.w700
             ),
             ),
           ),
           Center(
             child: Padding(
-              padding: const EdgeInsets.fromLTRB(40, 25, 40, 10),
+              padding: const EdgeInsets.fromLTRB(50, 20, 50, 10),
               child: TextField(
+                showCursor: true,
                 enableInteractiveSelection: true,
+                decoration: InputDecoration(
+                      border: OutlineInputBorder(borderRadius: BorderRadius.circular(40 )),
+                      prefixIcon: Icon(Icons.search),
+                      prefixIconColor: Colors.grey,
+                      hintText: 'Cari Kesukaanmu',
+                ),
               ),
             ),
           ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 15, 250, 0),
+            child: Text('Kategori',textAlign: TextAlign.start,
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500
+            ),),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(50, 25, 50, 0),
+            child: Container(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    // crossAxisAlignment: cros,
+                    children: [
+                      ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white
+                      ), 
+                      child: CircleAvatar()),
+                        ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white
+                      ), 
+                      child: CircleAvatar()),
+                       ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white
+                      ), 
+                      child: CircleAvatar()),
+                        ElevatedButton(onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white
+                      ), 
+                      child: CircleAvatar()),
+                    ],
+                  ),
+                  Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: cros,
+              children: [
+                ElevatedButton(onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white
+                ), 
+                child: CircleAvatar()),
+                  ElevatedButton(onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white
+                ), 
+                child: CircleAvatar()),
+                 ElevatedButton(onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white
+                ), 
+                child: CircleAvatar()),
+                  ElevatedButton(onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white
+                ), 
+                child: CircleAvatar()),
+              ],
+            ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 20, 200, 0),
+            child: Text('Tempat Wisata',
+            style: TextStyle(
+              fontSize: 17,
+              fontWeight: FontWeight.w400
+            ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(10, 20, 10, 0),
+            child: ListView(),
+          )
         ],
       ),
       )
