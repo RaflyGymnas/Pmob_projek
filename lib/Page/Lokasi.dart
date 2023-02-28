dd9b976c9c6df8480a96dd9e17bec983f824769e
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-02-28 12:39:22 +0700

    Ini buat page lokasi

--- lib/Page/Lokasi.dart
+++ lib/Page/Lokasi.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class lokasi extends StatelessWidget {
+  const lokasi({super.key});
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lokasi'),
+        ),
+        body: Center(
+          child: Text('mamak'),
+        ),
+      ),
+    );
+  }
+}
