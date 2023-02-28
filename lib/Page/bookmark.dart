9ab4a48b102f116f8d8c382ebc70b455bc962a1d
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-02-28 12:38:23 +0700

    Ini buat page bookmark

---
+++ lib/Page/bookmark.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class bookmark extends StatelessWidget {
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Tandai'),
+        ),
+        body: Center(
+          child: Text('mamak'),
+        ),
+      ),
+    );
+  }
+}
