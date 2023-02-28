c964f2282dc16d887ba0ce230f3c6dc791576be6
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-02-28 12:35:03 +0700

    Ini navbar nya fix

---
+++ lib/NavBar.dart
@@ -0,0 +1,49 @@
+import 'package:floating_navbar/floating_navbar_item.dart';
+import 'package:flutter/material.dart';
+import 'package:projek_pmob/Page/Lokasi.dart';
+import 'package:projek_pmob/Page/Profile.dart';
+import 'package:projek_pmob/Page/bookmark.dart';
+import 'package:projek_pmob/Page/dashboard.dart';
+import 'package:floating_navbar/floating_navbar.dart';
+
+class NavBar extends StatefulWidget {
+  int Index = 0;
+  List Page = [dashboardpage(), const lokasi(), bookmark(), const Profile()];
+
+  NavBar({super.key});
+
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.Page[widget.Index],
+      bottomNavigationBar: FloatingNavBar(
+        resizeToAvoidBottomInset: false,
+        color: Colors.lightBlue,
+        selectedIconColor: Colors.white,
+        unselectedIconColor: Colors.white.withOpacity(0.6),
+        items: [
+          FloatingNavBarItem(
+              iconData: Icons.home_outlined,
+              page: dashboardpage(),
+              title: 'Home'),
+          FloatingNavBarItem(
+              iconData: Icons.share_location, page: lokasi(), title: 'Lokasi'),
+          FloatingNavBarItem(
+              iconData: Icons.bookmark, page: bookmark(), title: 'Ditandai'),
+          FloatingNavBarItem(
+              iconData: Icons.person_4_rounded,
+              page: Profile(),
+              title: 'Profil'),
+        ],
+        horizontalPadding: 20.0,
+        hapticFeedback: true,
+        showTitle: true,
+      ),
+    );
+  }
+}
