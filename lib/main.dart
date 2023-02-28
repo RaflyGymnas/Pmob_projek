52f18f570cdf380fde7de3f17ce705b9e9950ead
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-02-28 12:36:19 +0700

    Main fix

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:projek_pmob/Page/Profile.dart';
 import 'package:projek_pmob/Page/dashboard.dart';
 import 'package:flutter/src/rendering/box.dart';
 
+import 'NavBar.dart';
+
 
 void main() {
   runApp(MyApp());
@@ -12,6 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Profile();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: NavBar()
+    );
   }
 }
