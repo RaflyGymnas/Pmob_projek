241cfa7f8458e3128d86ce63972f378e3c027e82
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-03-01 21:03:41 +0700

    Page syarat dan ketentuan

---
+++ lib/Page/SnK.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SyaratnKetentuan extends StatelessWidget {
+  const SyaratnKetentuan ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Syarat dan Ketentuan'),
+        ),
+        body: Center(
+          child: Text('mamak'),
+        ),
+      ),
+    );
+  }
+}
