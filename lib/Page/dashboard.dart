c2ca295b37ef6300b5c3cb6749144deaf62e0453
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-03-01 15:06:29 +0700

    Ini fix

--- lib/Page/dashboard.dart
+++ lib/Page/dashboard.dart
@@ -416,7 +416,7 @@ class Information extends StatelessWidget {
                             color: Colors.orange,
                       ),
                     ),
-                  ))
+                  )),
                 ],
               ),
             )),
