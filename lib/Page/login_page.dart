7316718999f4c9f945399f79753ede2a007d47ab
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-03-14 09:22:49 +0700

    masih otw

---
+++ lib/Page/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class Login_page extends StatelessWidget {
+  const Login_page({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ,
+    );
+  }
+}
