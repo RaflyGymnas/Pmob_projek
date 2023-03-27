0de457e2ec546e4b7652cd338449787f173aaac2
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-03-27 20:57:55 +0700

    Ini masih ada yg error

--- lib/Page/Lokasi.dart
+++ lib/Page/Lokasi.dart
@@ -3,27 +3,65 @@ import 'package:flutter/material.dart';
 class lokasi extends StatelessWidget {
   const lokasi({super.key});
 
- @override
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-        elevation: 0,
-        toolbarHeight: 50,
-        backgroundColor: Colors.white,
-        title: Text('Lokasi',
-        style: TextStyle(
-          fontFamily: 'Play-Regular.ttf',
-          fontSize: 20,
-          fontWeight: FontWeight.w500,
-          color: Colors.black
-        ),),
-        ),
-        body: Center(
-          child: Text('Hola'),
-        ),
+       body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            backgroundColor: Colors.white,
+            floating: true,
+            pinned: true,
+            snap: false,
+            centerTitle: false,
+            title: const Text('Lokasi',
+            style: TextStyle(
+              fontFamily: "Play.ttf",
+              color: Colors.black
+            ),),
+            bottom: AppBar(
+              elevation: 0,
+              toolbarHeight: 60,
+              backgroundColor: Colors.white,
+              title: Center(
+                child: Center(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        hintText: 'Search for something',
+                        prefixIcon: Icon(Icons.search),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(40)
+                        )
+                        ),
+                  ),
+                  
+                ),
+              ),
+            ),
+          ),
+          // Other Sliver Widgets
+          SliverList(
+            delegate: SliverChildListDelegate([
+              TabBar(tabs: )
+              SizedBox(
+                height: 400,
+                child: Center(
+                  child: Text(
+                    'This is an awesome shopping platform',
+                  ),
+                ),
+              ),
+              Container(
+                height: 1000,
+                color: Colors.pink,
+              ),
+            ]),
+          ),
+        ],
       ),
+    )
     );
   }
 }
