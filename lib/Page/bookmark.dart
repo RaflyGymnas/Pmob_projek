593d9c907a8a1bd0c0211d4f283046e77283eccf
RaflyGymnas <rafly.gymnastiar.zulmi-2021@fst.unair.ac.id>
2023-03-27 23:02:16 +0700

    Bookmark sementara kek gini dlu

--- lib/Page/bookmark.dart
+++ lib/Page/bookmark.dart
@@ -5,12 +5,38 @@ class bookmark extends StatelessWidget {
  @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Tandai'),
+          elevation: 0,
+          backgroundColor: Colors.white,
+          title: Text('Tandai Tempat',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Play-Regular.ttf'
+          ),),
         ),
-        body: Center(
-          child: Text('mamak'),
+        body: Padding(
+          padding: const EdgeInsets.only(top: 100.0),
+          child: Column(
+            children: [
+              Center(
+                child: Text('Belum Ada Tempat Ditandai',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w700,
+                  color: Colors.black
+                ),),
+              ),
+              Center(
+                child: Text('Jelajahi Tempat yang anda sukai',
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.black
+                ),),
+              ),
+            ],
+          ),
         ),
       ),
     );
