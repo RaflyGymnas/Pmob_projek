c9ab71e84f28c15b01039de3d1ad35286330ed8f
annisaisaura <annisa.isaura.alfiany-2021@fst.unair.ac.id>
2023-03-28 02:31:50 +0700

    delete-file

--- lib/constants.dart
+++
@@ -1,4 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kPrimaryColor = Color(0xFF5B84D6);
-const kPrimaryLightColor = Color(0xFFF3F3F3);
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:projek_pmob/Page/Profile.dart';
+import 'package:projek_pmob/Page/bookmark.dart';
+import 'package:projek_pmob/Page/Lokasi.dart';
 import 'package:projek_pmob/Page/dashboard.dart';
-import 'package:flutter/src/rendering/box.dart';
 import 'package:projek_pmob/Page/login_page.dart';
 import 'package:projek_pmob/Page/onboarding.dart';
-import 'package:projek_pmob/components/already_have_an_account_acheck.dart';
+import 'package:flutter/src/rendering/box.dart';
 import 'NavBar.dart';
 
 void main() {
